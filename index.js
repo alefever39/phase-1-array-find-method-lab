b2df180fc5c093d87353a571d900fe56ba3a72d7
Allie LeFever <alefever39@gmail.com>
2022-05-17 21:17:08 -0600

    commit tree c77d99326ca0eabf9a49f0c8a83968991e085c5f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// const record = [
+//     {year: '2015', result: 'L'},
+//     {year: '2014', result: 'N/A'},
+//     {year: '2013', result: 'W'},
+// ]
+
+function superbowlWin(array) {
+    const newObject = array.find(item => item.result === 'W')
+    if (typeof newObject === 'object') {
+        return newObject.year;
+    }
+}
+
+// console.log(superbowlWin(record));
